4133da23ab298841451c1961716a5a32faa43a9f
Elte Hupkes <eltehupkes@srxp.com>
2015-02-04 23:31:01 +0100

    Config for last test

--- build/evolve_simple.sh
+++ build/evolve_simple.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./robogen-evolver 1 results/my_first_evolution_experiment ../examples/stressEvolConf.txt
+./robogen-evolver 1 results/my_first_evolution_experiment "../examples/stress-1/evolConf.txt"
